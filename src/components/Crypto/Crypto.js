db2b678b3af1d091bdee33f970fbc0c27c1d7942
T1maty <timamy666@gmail.com>
2023-08-08 23:06:29 +0300

    Refactoring

--- src/components/Crypto/Crypto.js
+++ src/components/Crypto/Crypto.js
@@ -1,5 +1,4 @@
 import "../css/main.css";
-import "../fonts/font-awesome-4.7.0/css/font-awesome.min.css";
 import "../css/util.css";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
