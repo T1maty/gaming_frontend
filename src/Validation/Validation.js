a588a182d43b4dd3a704396efe50936b2d5a4649
T1maty <timamy666@gmail.com>
2023-08-13 20:41:04 +0300

    Add Validation in Order

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -6,6 +6,19 @@ const errors = {}
 
     const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]{2,6}$/;
 
+    const postindex_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]{2,6}$/;
+
+    const separation_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]{2,6}$/;
+
+    const adrress_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]{2,6}$/;
+
+    const firstname_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]{2,6}$/;
+
+    const lastname_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]{2,6}$/;
+
+
+
+
     const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/;
 
 
@@ -16,8 +29,39 @@ const errors = {}
 
     if (values.email === ""){
         errors.email = 'Email is  Required';
-    }else if (!password_pattern.test(values.email)) {
+    }else if (!email_pattern.test(values.email)) {
         errors.email = "Email did'nt match"
+    }
+
+    if (values.postindex === ""){
+        errors.postindex = 'Post Index is  Required';
+    }else if (!postindex_pattern.test(values.postindex)) {
+        errors.postindex = "Post Index did'nt match"
+    }
+
+    if (values.address === ""){
+        errors.address = 'Address is  Required';
+    }else if (!adrress_pattern.test(values.address)) {
+        errors.address = "Address did'nt match"
+    }
+
+    if (values.separation === ""){
+        errors.separation = 'Separation is  Required';
+    }else if (!separation_pattern.test(values.separation)) {
+        errors.separation = "Separation did'nt match"
+    }
+
+    if (values.firstname === ""){
+        errors.firstname = 'First Name  is  Required';
+    }else if (!firstname_pattern.test(values.firstname)) {
+        errors.firstname = "First Name did'nt match"
+    }
+
+
+    if (values.lastname === ""){
+        errors.lastname = 'Last Name  is  Required';
+    }else if (!lastname_pattern.test(values.lastname)) {
+        errors.lastname = "Last Name did'nt match"
     }
 
 
--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,6 +3,7 @@ import "../fonts/font-awesome-4.7.0/css/font-awesome.min.css";
 import "../css/util.css";
 import axios from "axios";
 import {useState} from "react";
+import Validation from "../../Validation/Validation";
 
 const Order = () => {
 
@@ -23,44 +24,77 @@ const Order = () => {
             console.log(error);
         });
 
+
+
+    const [values, setValues] = useState ({
+        firtsname:'',
+        lastname:'',
+        postindex:'',
+        email:'',
+        address:'',
+        separation:''
+    })
+
+    const [errors, setErrors] = useState({})
+
+    function handleInput(event) {
+        const newObject = {...values,[event.target.username]: event.target.value}
+        setValues(newObject)
+    }
+
+    function handleValidation(event){
+        event.preventDefault();
+        setErrors(Validation(values));
+    }
+
     return (
         <div className="limiter">
             <div className="container-login100">
                 <div className="wrap-login100">
-                    <form className="login100-form validate-form p-l-55 p-r-55 p-t-178" >
+                    <form className="login100-form validate-form p-l-55 p-r-55 p-t-178" onSubmit={handleValidation} >
                     <span className="login100-form-title">
 						Order Product
 					</span>
 
                         <div className="wrap-input100 validate-input m-b-16" data-validate="Please enter First Name">
-                            <input className="input100" type="text" name="firstname" placeholder="FirstName" />
+                            <input className="input100" type="text" name="firstname" placeholder="FirstName"  onChange={handleInput}/>
                             <span className="focus-input100"></span>
+                            {errors.firstname && <p style={{color:"red"}}>{errors.firstname}</p>}
                         </div>
 
                         <div className="wrap-input100 validate-input m-b-16" data-validate="Please enter Last Name">
-                            <input className="input100" type="text" name="lastname" placeholder="LastName" />
+                            <input className="input100" type="text" name="lastname" placeholder="LastName" onChange={handleInput} />
                             <span className="focus-input100"></span>
+                            {errors.lastname && <p style={{color:"red"}}>{errors.firstname}</p>}
+
                         </div>
 
                         <div className="wrap-input100 validate-input m-b-16" data-validate="Please enter Post Index">
-                            <input className="input100" type="text" name="postindex" placeholder="Post Index" />
+                            <input className="input100" type="text" name="postindex" placeholder="Post Index" onChange={handleInput} />
                             <span className="focus-input100"></span>
+                            {errors.postindex && <p style={{color:"red"}}>{errors.postindex}</p>}
+
                         </div>
                         <div className="wrap-input100 validate-input m-b-16" data-validate="Please enter Email">
-                            <input className="input100" type="text" name="email" placeholder="Email" />
+                            <input className="input100" type="text" name="email" placeholder="Email"  onChange={handleInput}/>
                             <span className="focus-input100"></span>
+                            {errors.email && <p style={{color:"red"}}>{errors.email}</p>}
+
                         </div>
                         <div className="wrap-input100 validate-input m-b-16" data-validate="Please enter Address">
-                            <input className="input100" type="text" name="address" placeholder="Address" />
+                            <input className="input100" type="text" name="address" placeholder="Address"  onChange={handleInput}/>
                             <span className="focus-input100"></span>
+                            {errors.address && <p style={{color:"red"}}>{errors.address}</p>}
+
                         </div>
                         <div className="wrap-input100 validate-input m-b-16" data-validate="Please enter Separation">
-                            <input className="input100" type="text" name="separation" placeholder="Separation" />
+                            <input className="input100" type="text" name="separation" placeholder="Separation"  onChange={handleInput}/>
                             <span className="focus-input100"></span>
+                            {errors.separation && <p style={{color:"red"}}>{errors.separation}</p>}
                         </div>
 
                         <div className="container-login100-form-btn">
-                            <a href="/complete-order" className="login100-form-btn">
+                            <a href="/crypto" className="login100-form-btn">
                                 Complete Order
                             </a>
                         </div>
